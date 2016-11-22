7138a2232eb808a6ba33f2a2bff7b8b14de94454
Javier Pena <javier@javierdev.com>
2016-11-22 17:28:09 -0600

    Add no items notice for EventsList

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import Card from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import Event from 'material-ui/svg-icons/action/event';
 
 import { startEventsRetrieval } from 'eventsActions';
 import AddEventButton from 'AddEventButton';
 import { setAppbarTitle } from 'appbarTitleActions';
 import EventListItem from 'EventListItem';
+
+const noItemsStyle = {
+  padding: '10px',
+  minHeight: '50vh',
+  textAlign: 'center'
+};
 
 class EventsList extends React.Component {
   constructor(props) {
@@ -19,8 +29,23 @@ class EventsList extends React.Component {
   renderList() {
     const {events} = this.props;
 
-    if (events) {
-      return events.map((event) => <EventListItem key={event.spacedId} {...event} />);
+    if (Array.isArray(events) && events.length) {
+      return events.map((event) => {
+        return <EventListItem key={event.spacedId} {...event} />;
+      });
+    } else if (Array.isArray(events) && !events.legnth) {
+      return (
+        <Card style={noItemsStyle}>
+          <h2>No items</h2>
+          <Link to="/events/new">
+            <RaisedButton
+              label="Add Spaced Event"
+              primary={true}
+              icon={<Event />}
+            />
+          </Link>
+        </Card>
+      );
     } else {
       return null;
     }
